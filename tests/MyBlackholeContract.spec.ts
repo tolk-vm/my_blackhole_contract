02ae4694aced584dd5b36e5576f9ea7e8d8d87c2
Aleksandr Kirsanov <unserialize.alias@gmail.com>
2024-01-27 19:08:50 +0300

    code of contract and its tests

--- tests/MyBlackholeContract.spec.ts
+++ tests/MyBlackholeContract.spec.ts
@@ -12,20 +12,25 @@ describe('MyBlackholeContract', () => {
     });
 
     let blockchain: Blockchain;
-    let deployer: SandboxContract<TreasuryContract>;
+    let deployerWallet: SandboxContract<TreasuryContract>;
+    let ownerWallet: SandboxContract<TreasuryContract>;
     let myBlackholeContract: SandboxContract<MyBlackholeContract>;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        myBlackholeContract = blockchain.openContract(MyBlackholeContract.createFromConfig({}, code));
+        ownerWallet = await blockchain.treasury("owner")
 
-        deployer = await blockchain.treasury('deployer');
+        myBlackholeContract = blockchain.openContract(MyBlackholeContract.createFromConfig({
+            counter_value: 0,
+            owner_address: ownerWallet.address,
+        }, code));
 
-        const deployResult = await myBlackholeContract.sendDeploy(deployer.getSender(), toNano('0.05'));
+        deployerWallet = await blockchain.treasury('deployer');
 
+        const deployResult = await myBlackholeContract.sendDeploy(deployerWallet.getSender(), toNano('0.003'));
         expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
+            from: deployerWallet.address,
             to: myBlackholeContract.address,
             deploy: true,
             success: true,
@@ -36,4 +41,90 @@ describe('MyBlackholeContract', () => {
         // the check is done inside beforeEach
         // blockchain and myBlackholeContract are ready to use
     });
+
+    it("should add 2 and 3 and return 5", async () => {
+        const senderWallet = await blockchain.treasury('sender');
+
+        await myBlackholeContract.sendIncrement(senderWallet.getSender(), toNano("0.05"), 2);
+        await myBlackholeContract.sendIncrement(senderWallet.getSender(), toNano("0.15"), 3);
+
+        const state = await myBlackholeContract.getFullState();
+        expect(state.counter_value).toEqual(2 + 3);
+    });
+
+    it("should return correct sender's address", async () => {
+        const senderWallet = await blockchain.treasury('sender');
+
+        const sendResult = await myBlackholeContract.sendIncrement(senderWallet.getSender(), toNano("0.05"), 2);
+        expect(sendResult.transactions).toHaveTransaction({
+            from: senderWallet.address,
+            to: myBlackholeContract.address,
+            success: true,
+        })
+
+        const state = await myBlackholeContract.getFullState();
+        expect(state.recent_inc_addr!.toString()).toBe(senderWallet.address.toString());
+        expect(state.owner_addr.toString()).toBe(ownerWallet.address.toString());
+    })
+
+    it("should deposit funds", async () => {
+        const senderWallet = await blockchain.treasury('sender');
+
+        await myBlackholeContract.sendDeposit(senderWallet.getSender(), toNano("5"));
+
+        const balance = await myBlackholeContract.getBalance();
+        expect(balance).toBeGreaterThan(toNano("4.99"));
+
+        const dump = await myBlackholeContract.getFullState();
+        expect(dump.recent_deposit_addr!.toString()).toEqual(senderWallet.address.toString())
+    })
+
+    it("should withdraw 3 TON to owner", async () => {
+        const senderWallet = await blockchain.treasury('sender');
+        const ownerBalanceStart = await ownerWallet.getBalance()
+
+        await myBlackholeContract.sendDeposit(senderWallet.getSender(), toNano("2"));
+        await myBlackholeContract.sendIncrement(senderWallet.getSender(), toNano("2"), 100);
+        const sendResult = await myBlackholeContract.sendWithdraw(ownerWallet.getSender(), toNano("0.003"), toNano(3));
+        expect(sendResult.transactions).toHaveTransaction({
+            from: myBlackholeContract.address,
+            to: ownerWallet.address,
+            success: true,
+            value: toNano(3),
+        })
+
+        expect(await myBlackholeContract.getBalance()).toBeGreaterThan(toNano("0.5"))
+        expect(await myBlackholeContract.getBalance()).toBeLessThan(toNano("1"))
+
+        const ownerBalanceDelta = await ownerWallet.getBalance() - ownerBalanceStart
+        expect(ownerBalanceDelta >= toNano("2.9") && ownerBalanceDelta <= toNano("3")).toBeTruthy()
+    })
+
+    it("should fail to withdraw not to an owner", async () => {
+        const senderWallet = await blockchain.treasury('sender');
+
+        await myBlackholeContract.sendDeposit(senderWallet.getSender(), toNano("2"));
+
+        const sendResult = await myBlackholeContract.sendWithdraw(senderWallet.getSender(), toNano("0.05"), toNano(3));
+        expect(sendResult.transactions).toHaveTransaction({
+            from: senderWallet.address,
+            to: myBlackholeContract.address,
+            success: false,
+            exitCode: 777,
+        })
+    })
+
+    it("should fail to withdraw more than contains", async () => {
+        const senderWallet = await blockchain.treasury('sender');
+
+        await myBlackholeContract.sendDeposit(senderWallet.getSender(), toNano("2"));
+
+        const sendResult = await myBlackholeContract.sendWithdraw(ownerWallet.getSender(), toNano("0.05"), toNano(3));
+        expect(sendResult.transactions).toHaveTransaction({
+            from: ownerWallet.address,
+            to: myBlackholeContract.address,
+            success: false,
+            exitCode: 778,
+        })
+    })
 });
--- wrappers/MyBlackholeContract.ts
+++ wrappers/MyBlackholeContract.ts
@@ -1,17 +1,22 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
-export type MyBlackholeContractConfig = {};
+export type MyBlackholeContractConfig = {
+    counter_value: number,
+    owner_address: Address,
+};
 
+// see my_store_data() in my_blackhole_contract.fc
 export function myBlackholeContractConfigToCell(config: MyBlackholeContractConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+      .storeUint(config.counter_value, 32)
+      .storeAddress(null)
+      .storeAddress(null)
+      .storeAddress(config.owner_address)
+      .endCell();
 }
 
 export class MyBlackholeContract implements Contract {
     constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
-
-    static createFromAddress(address: Address) {
-        return new MyBlackholeContract(address);
-    }
 
     static createFromConfig(config: MyBlackholeContractConfig, code: Cell, workchain = 0) {
         const data = myBlackholeContractConfigToCell(config);
@@ -20,10 +25,53 @@ export class MyBlackholeContract implements Contract {
     }
 
     async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        let opcode = 2
         await provider.internal(via, {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
+            body: beginCell().storeUint(opcode, 32).endCell(),
         });
+    }
+
+    async sendIncrement(provider: ContractProvider, sender: Sender, valueMoney: bigint, incBy: number) {
+        let opcode = 1
+        return await provider.internal(sender, {
+            value: valueMoney,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(opcode, 32).storeUint(incBy, 32).endCell(),
+        });
+    }
+
+    async sendDeposit(provider: ContractProvider, sender: Sender, valueMoney: bigint) {
+        let opcode = 2
+        return await provider.internal(sender, {
+            value: valueMoney,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(opcode, 32).endCell(),
+        });
+    }
+
+    async sendWithdraw(provider: ContractProvider, sender: Sender, valueMoney: bigint, toWithdraw: bigint) {
+        let opcode = 3
+        return await provider.internal(sender, {
+            value: valueMoney,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(opcode, 32).storeCoins(toWithdraw).endCell(),
+        });
+    }
+
+    async getFullState(provider: ContractProvider) {
+        const { stack: r } = await provider.get("dump_full_state", []);
+        return {
+            counter_value: r.readNumber(),
+            recent_inc_addr: r.readAddressOpt(),
+            recent_deposit_addr: r.readAddressOpt(),
+            owner_addr: r.readAddress(),
+        };
+    }
+
+    async getBalance(provider: ContractProvider) {
+        const { stack: r } = await provider.get('show_balance', [])
+        return r.readNumber()
     }
 }
